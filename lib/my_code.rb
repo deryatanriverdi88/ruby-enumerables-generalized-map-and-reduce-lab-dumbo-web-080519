878401cbf02c0b7ce2570a2d27e7bd9ec2443af7
Derya Tanriverdi <deryatanriverdi_@hotmail.com>
2019-08-05 16:13:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
-# Your Code Here
+negative = lambda { |num| num * -1}
+original = lambda { |num| num }
+multiply = lambda { |num| num * 2}
+square = lambda { |num| num ** 2}
+
+def custom_map(array, choice_of_lambda)
+  i = 0
+  result = []
+  while i < array.length
+    result << choice_of_lambda.call(array[i])
+    i += 1
+  end
+  result
+end
+
